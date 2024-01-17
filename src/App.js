975f68f1c7a4b5b803959214e31482ccffeb1703
CabsT <133862761+CabsT@users.noreply.github.com>
2024-01-17 20:20:57 +0200

    Added a link to my github repository

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-
-import './App.css';
+import "./App.css";
 import SearchCityForm from "./WeatherSearchForm";
-
-
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
       <SearchCityForm />
+      <p>
+        This is the link to my{" "}
+        <a
+          href="https://github.com/CabsT/react_weatherapp"
+          target="_blank"
+          rel="noreferrer"
+        >
+          github repository
+        </a>
+      </p>
     </div>
   );
 }
